ed808da1fbf60abfb77c484b460b307e11116b70
Micoko <112613372+Micoko@users.noreply.github.com>
2023-11-23 11:55:24 +0900

    init

---
+++ src/main/java/com/example/gaziworld/GaziworldApplication.java
@@ -0,0 +1,13 @@
+package com.example.gaziworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaziworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaziworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gaziworld/GaziworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gaziworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaziworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
